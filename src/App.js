7ff9e8f0a8cc9a86f202aa8dee82f2350deec517
Hikmahx <hikmayousuph@gmail.com>
2021-12-09 07:14:21 +0100

    changed into arrow function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
 
-function App() {
-  return <div className='App'>
-  </div>;
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
