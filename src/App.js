07846130eecc08dfa2a6809a5e3a9baad8724b54
Hikmahx <hikmayousuph@gmail.com>
2021-12-12 20:44:01 +0100

    components imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="App w-full relative">
+      <Navbar/>
+      <Hero/>
+      <Skills/>
     </div>
   )
 }
