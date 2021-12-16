75b01bb826dcc6fbb80701b8f90fb1b119cdde47
Hikmahx <hikmayousuph@gmail.com>
2021-12-16 23:19:33 +0100

    project component imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Skills/>
+      <Projects/>
     </div>
   )
 }
