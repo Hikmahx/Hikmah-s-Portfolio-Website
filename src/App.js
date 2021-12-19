d57979b5ea5dfca4f80593809f5e996f274dbf3b
Hikmahx <hikmayousuph@gmail.com>
2021-12-19 18:59:05 +0100

    component  imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   )
 }
