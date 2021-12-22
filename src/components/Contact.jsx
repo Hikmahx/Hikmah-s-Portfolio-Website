912c5ce8ab524a7ba2905773c4681237f3ecd355
Hikmahx <hikmayousuph@gmail.com>
2021-12-22 01:27:55 +0100

    form updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,24 +8,22 @@ const Contact = () => {
       <h2 className="flex justify-center font-bold lg:text-4xl mb-6">Contact</h2>
       <div className="form-wrapper bg-white w-full max-w-2xl md:max-w-3xl py-16 px-6 sm:px-12">
         <form action="mailto:hikmayousuph@gmail.com" method="POST" encType="multipart/form-data" name="contactForm" className="" id="contact-form">
-            <div class="input-group my-4 border-b-2">
-              <input type="text" id="name" name="Name" aria-label="name" className="field focus:outline-none w-full text-black" placeholder="Name"/>            
+            <div className="input-group my-4 border-b-2">
+              <input type="text" id="name" name="Name" aria-label="name" className="field focus:outline-none w-full text-black" placeholder="Name" required/>            
             </div>
-            <div class="input-group my-4 border-b-2">
-              <input type="text" id="email" name="email" aria-label="email" className="field focus:outline-none w-full text-black" placeholder="Email Address"/>            
+            <div className="input-group my-4 border-b-2">
+              <input type="email" id="email" name="email" aria-label="email" className="field focus:outline-none w-full text-black" placeholder="Email Address" required/>            
             </div>
-            <div class="input-group my-4 border-b-2">
-              <textarea  type="text" id="message" name="message" aria-label="message" className="field focus:outline-none w-full text-black" placeholder="Message"></textarea>
+            <div className="input-group my-4 border-b-2">
+              <textarea  type="text" id="message" name="message" aria-label="message" rows="3" className="field focus:outline-none w-full text-black" placeholder="Message" required></textarea>
             </div>
             <div className="input-group flex justify-end">
-              <input style={{background:'#0ad7c5'}} type="submit" className="submit-btn w-full md:w-auto px-6 py-4 mt-6 text-black transition-colors cursor-pointer hover:text-white"  value="Submit"/>
+              <button style={{background:'#0ad7c5'}} type="submit" className="submit-btn w-full md:w-auto px-6 py-4 mt-6 text-black transition-colors cursor-pointer hover:text-white">Submit</button>
             </div>
         </form>
       </div>
 
-    </section>
-      
-    
+    </section>  
   )
 }
 
