2b50c70f4cec5369c9d05a7dc8f40a9153c6b7bd
Hikmahx <hikmayousuph@gmail.com>
2021-12-22 01:59:39 +0100

    contact form functioning well

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
     <section id="contact" style={{background:'#202c37'}} className="contact py-16 px-4 md:px-10 bg-gray-50 text-white flex flex-col items-center justify-center">
       <h2 className="flex justify-center font-bold lg:text-4xl mb-6">Contact</h2>
       <div className="form-wrapper bg-white w-full max-w-2xl md:max-w-3xl py-16 px-6 sm:px-12">
-        <form action="mailto:hikmayousuph@gmail.com" method="POST" encType="multipart/form-data" name="contactForm" className="" id="contact-form">
+        <form  action="https://formsubmit.co/hikmayousuph@gmail.com" method="POST">
             <div className="input-group my-4 border-b-2">
-              <input type="text" id="name" name="Name" aria-label="name" className="field focus:outline-none w-full text-black" placeholder="Name" required/>            
+              <input type="text" id="name" name="fullName" aria-label="name" className="field focus:outline-none w-full text-black" placeholder="Name" required/>            
             </div>
             <div className="input-group my-4 border-b-2">
               <input type="email" id="email" name="email" aria-label="email" className="field focus:outline-none w-full text-black" placeholder="Email Address" required/>            
@@ -17,6 +17,7 @@ const Contact = () => {
             <div className="input-group my-4 border-b-2">
               <textarea  type="text" id="message" name="message" aria-label="message" rows="3" className="field focus:outline-none w-full text-black" placeholder="Message" required></textarea>
             </div>
+            <input type="hidden" name="_subject" value="My website new submission!"/>
             <div className="input-group flex justify-end">
               <button style={{background:'#0ad7c5'}} type="submit" className="submit-btn w-full md:w-auto px-6 py-4 mt-6 text-black transition-colors cursor-pointer hover:text-white">Submit</button>
             </div>
