7bf11d60197efa0fe502d26a99ea37864738c2bb
Hikmahx <hikmayousuph@gmail.com>
2022-09-07 19:40:11 -1100

    project component updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -65,32 +65,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
-
-        <div className="project bg-white relative overflow-hidden rounded-sm w-72 lg:w-80 shadow-md mx-4 my-6">
-          <div className="img-wrapper relative w-72 lg:w-80 h-72 lg:h-80">
-            <img className="w-full h-full object-cover" src={require('../assets/projects-images/react-job-list-filtering-hikmahx.vercel.app_ (1).png')} alt="project-img" />
-          </div>
-          <div className="info p-6 flex flex-col justify-end">
-            <h3 className="font-bold uppercase mb-3">JOB FILTERING</h3>
-            <p className="mb-4">
-              Responsive single page website used to filter out jobs based on the button(s) clicked. 
-            </p>
-            <div className="skills flex flex-wrap items-start mb-14">
-              <div className="px-4 rounded-full m-1 bg-gray-200 text-gray-600">react</div>
-              <div className="px-4 rounded-full m-1 bg-gray-200 text-gray-600">sass</div>
-              <div className="px-4 rounded-full m-1 bg-gray-200 text-gray-600">JS</div>
-            </div>
-            <div className="links flex absolute bottom-6">
-              <a target="_blank" className="font-semibold m-2" href="https://github.com/Hikmahx/react-job-list-filtering" aria-label="github link">
-                <img className="w-6 h-6" src={require('../assets/github.svg')} alt="icon" />
-              </a>
-              <a target="_blank" className="font-semibold m-2" href="https://react-job-list-filtering-hikmahx.vercel.app/" aria-label="site link">
-                <img className="w-6 h-6" src={require('../assets/external-link.svg')} alt="icon" />
-              </a>
-            </div>
-          </div>
-        </div>
-
+        
         <div className="project bg-white relative overflow-hidden rounded-sm w-72 lg:w-80 shadow-md mx-4 my-6">
           <div className="img-wrapper relative w-72 lg:w-80 h-72 lg:h-80">
             <img className="w-full h-full object-cover" src={require('../assets/projects-images/space-tourism-hikmahx.vercel.app_.png')} alt="project-img" />
@@ -113,6 +88,31 @@ const Projects = () => {
                 <img className="w-6 h-6" src={require('../assets/github.svg')} alt="icon" />
               </a>
               <a target="_blank" className="font-semibold m-2" href="https://space-tourism-hikmahx.vercel.app/" aria-label="site link">
+                <img className="w-6 h-6" src={require('../assets/external-link.svg')} alt="icon" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="project bg-white relative overflow-hidden rounded-sm w-72 lg:w-80 shadow-md mx-4 my-6">
+          <div className="img-wrapper relative w-72 lg:w-80 h-72 lg:h-80">
+            <img className="w-full h-full object-cover" src={require('../assets/projects-images/react-job-list-filtering-hikmahx.vercel.app_ (1).png')} alt="project-img" />
+          </div>
+          <div className="info p-6 flex flex-col justify-end">
+            <h3 className="font-bold uppercase mb-3">JOB FILTERING</h3>
+            <p className="mb-4">
+              Responsive single page website used to filter out jobs based on the button(s) clicked. 
+            </p>
+            <div className="skills flex flex-wrap items-start mb-14">
+              <div className="px-4 rounded-full m-1 bg-gray-200 text-gray-600">react</div>
+              <div className="px-4 rounded-full m-1 bg-gray-200 text-gray-600">sass</div>
+              <div className="px-4 rounded-full m-1 bg-gray-200 text-gray-600">JS</div>
+            </div>
+            <div className="links flex absolute bottom-6">
+              <a target="_blank" className="font-semibold m-2" href="https://github.com/Hikmahx/react-job-list-filtering" aria-label="github link">
+                <img className="w-6 h-6" src={require('../assets/github.svg')} alt="icon" />
+              </a>
+              <a target="_blank" className="font-semibold m-2" href="https://react-job-list-filtering-hikmahx.vercel.app/" aria-label="site link">
                 <img className="w-6 h-6" src={require('../assets/external-link.svg')} alt="icon" />
               </a>
             </div>
