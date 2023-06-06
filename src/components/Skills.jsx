5fbfb437dfa3140d6758b0b59624dc9d49a63536
Hikmahx <hikmayousuph@gmail.com>
2023-06-06 20:41:01 +0100

    skills section updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,105 +6,28 @@ const Skills = () => {
   return (
     <section className="skills py-16 bg-gray-100 px-4 md:px-10 ">
       <h2 className="flex justify-center font-bold lg:text-4xl">My Skills</h2>
-
-    <div className="container flex overflow-x-scroll mx-auto">
-    <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/html.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">HTML</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/css.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">CSS</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/python.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Python</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/javascript.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">JavaScript</h3>
-        </div>
-      </div>      
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={reactIcon} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">React</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={bootstrap} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Bootstrap</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/git.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Git</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/github.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Github</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/sass.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Sass</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/tailwindcss.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Tailwind</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/vercel.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">Vercel</h3>
-        </div>
-      </div>
-      <div style={{boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)'}} className="skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded">
-        <div className="img-container w-28 h-32 lg:w-36 lg:h-40">
-          <img className="p-7" src={require('../assets/visual-studio-code.svg')} alt="icon" />
-        </div>
-        <div className="icon-name flex justify-center mb-3">
-          <h3 className="font-bold">VsCode</h3>
-        </div>
-      </div>
-    </div>
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-8 gap-x-2  marker:text-[#27d7c5] list-disc mx-auto items-center">
+        <li className="py-3">HTML</li>
+        <li className="py-3">CSS</li>
+        <li className="py-3">JavaScript</li>
+        <li className="py-3">TypeScript</li>
+        <li className="py-3">Python</li>
+        <li className="py-3">React</li>
+        <li className="py-3">Tailwind</li>
+        <li className="py-3">Bootstrap</li>
+        <li className="py-3">SCSS/SASS</li>
+        <li className="py-3">Git/GitHub</li>
+        <li className="py-3">Figma</li>
+        <li className="py-3">Next JS</li>
+        <li className="py-3">Redux</li>
+        <li className="py-3">Node JS</li>
+        <li className="py-3">Express JS</li>
+        <li className="py-3">MongoDB</li>
+        <li className="py-3">Django</li>
+        <li className="py-3">REST API</li>
+        <li className="py-3">Postman</li>
+        <li className="py-3">React Native</li>
+      </ul>
     </section>
   )
 }
