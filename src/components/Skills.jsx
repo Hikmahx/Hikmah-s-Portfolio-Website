ddacacd7dde7c5852168fb754f0d34f1840d7334
Hikmahx <hikmayousuph@gmail.com>
2025-08-31 17:10:25 +0100

    new skills added, skill ui reverted back to original design

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,33 +1,51 @@
 import React from 'react'
-import reactIcon from '../assets/react.svg'
-import bootstrap from '../assets/bootstrap.svg'
+
+const skills = [
+  // { name: 'HTML', icon: require('../assets/html.svg') },
+  // { name: 'CSS', icon: require('../assets/css.svg') },
+  { name: 'Python', icon: require('../assets/python.svg') },
+  // { name: 'JavaScript', icon: require('../assets/javascript.svg') },
+  { name: 'TypeScript', icon: require('../assets/typescript.svg') },
+  { name: 'React', icon: require('../assets/react.svg') },
+  { name: 'React Native', icon: require('../assets/react-native.svg') },
+  { name: 'Figma', icon: require('../assets/figma.svg') },
+  { name: 'Next.js', icon: require('../assets/nextjs.svg') },
+  { name: 'Redux', icon: require('../assets/redux.svg') },
+  { name: 'Git', icon: require('../assets/git.svg') },
+  { name: 'Github', icon: require('../assets/github.svg') },
+  { name: 'Sass', icon: require('../assets/sass.svg') },
+  { name: 'GraphQL', icon: require('../assets/graphql.svg') },
+  { name: 'MongoDB', icon: require('../assets/mongodb.svg') },
+  { name: 'Tailwind', icon: require('../assets/tailwindcss.svg') },
+  { name: 'Postman', icon: require('../assets/postman.svg') },
+  { name: 'Vercel', icon: require('../assets/vercel.svg') },
+  { name: 'VsCode', icon: require('../assets/visual-studio-code.svg') },
+  { name: 'Jest', icon: require('../assets/jest.svg') },
+  { name: 'Firebase', icon: require('../assets/firebase.svg') },
+  { name: 'MixPanel', icon: require('../assets/mixpanel.svg') },
+  { name: 'Bootstrap', icon: require('../assets/bootstrap.svg') },
+]
 
 const Skills = () => {
   return (
-    <section className="skills py-16 bg-gray-100 px-4 md:px-10 ">
-      <h2 className="flex justify-center font-bold lg:text-4xl">My Skills</h2>
-      <ul className="mt-3 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-8 gap-x-2  marker:text-[#27d7c5] list-disc mx-auto items-center">
-        <li className="py-3">HTML</li>
-        <li className="py-3">CSS</li>
-        <li className="py-3">JavaScript</li>
-        <li className="py-3">TypeScript</li>
-        <li className="py-3">Python</li>
-        <li className="py-3">React</li>
-        <li className="py-3">Tailwind</li>
-        <li className="py-3">Bootstrap</li>
-        <li className="py-3">SCSS/SASS</li>
-        <li className="py-3">Git/GitHub</li>
-        <li className="py-3">Figma</li>
-        <li className="py-3">Next JS</li>
-        <li className="py-3">Redux</li>
-        <li className="py-3">Node JS</li>
-        <li className="py-3">Express JS</li>
-        <li className="py-3">MongoDB</li>
-        <li className="py-3">Django</li>
-        <li className="py-3">REST API</li>
-        <li className="py-3">Postman</li>
-        <li className="py-3">React Native</li>
-      </ul>
+    <section className='skills py-16 bg-gray-100 px-4 md:px-10 '>
+      <h2 className='flex justify-center font-bold lg:text-4xl'>My Skills</h2>
+      <div className='container flex overflow-x-scroll mx-auto'>
+        {skills.map((skill, idx) => (
+          <div
+            key={skill.name}
+            style={{ boxShadow: '0 4px 10px 0 rgb(189 208 223 / 25%)' }}
+            className='skill mx-4 my-8 bg-white w-32 h-44 lg:w-40 lg:h-52 rounded'
+          >
+            <div className='img-container w-28 h-32 lg:w-36 lg:h-40'>
+              <img className='p-7' src={skill.icon} alt='icon' />
+            </div>
+            <div className='icon-name flex justify-center mb-3'>
+              <h3 className='font-bold'>{skill.name}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
